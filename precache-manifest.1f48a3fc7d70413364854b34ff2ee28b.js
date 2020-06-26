689b9b73d9ea5ca707d5d1f91a39619ad9eec0f6
unknown <pangeriz@outlook.com>
2020-06-26 20:13:44 +0100

    Updates

---
+++ precache-manifest.1f48a3fc7d70413364854b34ff2ee28b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f090f84e9127167f0911df3fae0c7821",
+    "url": "/demo-iv/index.html"
+  },
+  {
+    "revision": "f8e5fbc493e809c69adf",
+    "url": "/demo-iv/static/css/main.deea161a.chunk.css"
+  },
+  {
+    "revision": "4936462528cea4a8516d",
+    "url": "/demo-iv/static/js/2.1edf7f29.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/demo-iv/static/js/2.1edf7f29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8e5fbc493e809c69adf",
+    "url": "/demo-iv/static/js/main.04f5f16e.chunk.js"
+  },
+  {
+    "revision": "99fa8aa690055899fde6",
+    "url": "/demo-iv/static/js/runtime-main.17813182.js"
+  }
+]);
